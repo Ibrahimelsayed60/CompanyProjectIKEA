9549c27c6fe54f713f9664914625748f1c303c0a
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-09-27 13:31:00 -0700

    Part 05 Department Module (Entities, Configrations) and DbContext

---
+++ LinkDev.IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using LinkDev.IKEA.DAL.Models.Department;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Data
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext():base()
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = IKEA_G03; Trusted_Connection = True; TrustServerCertificate = True;");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
