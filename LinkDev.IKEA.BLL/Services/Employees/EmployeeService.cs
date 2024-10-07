4a6d84bd7ee09f8c43c393948861dfd30e272258
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-10-06 19:49:46 -0700

    Part 05 Employee Module - Service

---
+++ LinkDev.IKEA.BLL/Services/Employees/EmployeeService.cs
@@ -0,0 +1,119 @@
+﻿using LinkDev.IKEA.BLL.Models.Employees;
+using LinkDev.IKEA.DAL.Entities.Employees;
+using LinkDev.IKEA.DAL.Persistance.Repositories.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.BLL.Services.Employees
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeRepository employeeRepository) // Ask CLR For Creating Object from class implementing "IEmployeeRepository"
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public IEnumerable<EmployeeDto> GetAllEmployees()
+        {
+            return _employeeRepository.GetAllAsIQueryable().Select(employee => new EmployeeDto()
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Age = employee.Age,
+                Salary = employee.Salary,
+                IsActive = employee.IsActive,
+                Email = employee.Email,
+                Gender = nameof(employee.Gender),
+                EmployeeType = nameof(employee.EmployeeType),
+            });
+        }
+
+        public EmployeeDetailsDto? GetEmployeeById(int id)
+        {
+            var employee = _employeeRepository.Get(id);
+
+            if (employee is { })
+                return new EmployeeDetailsDto()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Age = employee.Age,
+                    Address = employee.Address,
+                    Salary = employee.Salary,
+                    IsActive = employee.IsActive,
+                    Email = employee.Email,
+                    PhoneNumber = employee.PhoneNumber,
+                    HiringDate = employee.HiringDate,
+                    Gender = nameof(employee.Gender),
+                    EmployeeType = nameof(employee.EmployeeType)
+                };
+
+            return null;
+        }
+
+        public int CreateEmployee(CreatedEmployeeDto employeeDto)
+        {
+            var employee = new Employee()
+            {
+                Name = employeeDto.Name,
+                Age = employeeDto.Age,
+                Address = employeeDto.Address,
+                Salary = employeeDto.Salary,
+                IsActive = employeeDto.IsActive,
+                Email = employeeDto.Email,
+                PhoneNumber = employeeDto.PhoneNumber,
+                HiringDate = employeeDto.HiringDate,
+                Gender = employeeDto.Gender,
+                EmployeeType = employeeDto.EmployeeType,
+                CreatedBy = 1,
+                LastModifiedBy = 1,
+                LastModifiedOn = DateTime.UtcNow
+            };
+
+            return _employeeRepository.Add(employee);
+        }
+
+        public int UpdateEmployee(UpdatedEmployeeDto employeeDto)
+        {
+            var employee = new Employee()
+            {
+                Id = employeeDto.Id,
+                Name = employeeDto.Name,
+                Age = employeeDto.Age,
+                Address = employeeDto.Address,
+                Salary = employeeDto.Salary,
+                IsActive = employeeDto.IsActive,
+                Email = employeeDto.Email,
+                PhoneNumber = employeeDto.PhoneNumber,
+                HiringDate = employeeDto.HiringDate,
+                Gender = employeeDto.Gender,
+                EmployeeType = employeeDto.EmployeeType,
+                CreatedBy = 1,
+                LastModifiedBy = 1,
+                LastModifiedOn = DateTime.UtcNow
+            };
+
+            return _employeeRepository.Update(employee);
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+            var employee = _employeeRepository.Get(id);
+
+            if(employee is { })
+                return _employeeRepository.Delete(employee) > 0;
+
+            return false;
+
+        }
+
+        
+
+        
+    }
+}
---
+++ LinkDev.IKEA.BLL/Services/Employees/IEmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using LinkDev.IKEA.BLL.Models.Departments;
+using LinkDev.IKEA.BLL.Models.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.BLL.Services.Employees
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<EmployeeDto> GetAllEmployees();
+
+        EmployeeDetailsDto? GetEmployeeById(int id);
+
+        int CreateEmployee(CreatedEmployeeDto employeeDto);
+
+        int UpdateEmployee(UpdatedEmployeeDto employeeDto);
+
+        bool DeleteEmployee(int id);
+
+    }
+}
