7ed93db25d07b631ea7b72a63fa1d8337437be5f
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-10-16 16:38:07 -0700

    Part 09 Account Controller

---
+++ LinkDev.IKEA.PL/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkDev.IKEA.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Register
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // Login
+        // Sign out
+        // Forget Password
+        // Reset Password
+    }
+}
